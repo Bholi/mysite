524ac6d298aed22842d5bebc93c5ae3fe266ec93
Bholi <najmulbro123@gmail.com>
2024-12-10 17:39:43 +0545

    name in profile

--- base/models.py
+++ base/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=100,null=True,blank=True)
     nuber = models.IntegerField(null=True,blank=True)
+    def __str__(self):
+        return self.name
